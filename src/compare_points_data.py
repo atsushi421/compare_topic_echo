95e3145cacfafb7576d2c8353dfecfee66d85d86
atsushi421 <atsushi.yano.2@tier4.jp>
2023-09-11 20:44:48 +0900

    fix

--- src/compare_points_data.py
+++ src/compare_points_data.py
@@ -5,11 +5,17 @@ from tqdm import tqdm
 
 
 class Point:
-    def __init__(self, x: float, y: float, z: float, intensity: float):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.intensity = intensity
+    def __init__(self, x: list[int], y: list[int], z: list[int], intensity: list[int]):
+        def to_float(chars: list[int]) -> float:
+            bytes_repr = bytes(chars)
+            float_value = struct.unpack('f', bytes_repr)[0]
+
+            return float_value
+
+        self.x = to_float(x)
+        self.y = to_float(y)
+        self.z = to_float(z)
+        self.intensity = to_float(intensity)
 
     def __eq__(self, other):
         FACTOR = 100000000.0
@@ -20,16 +26,6 @@ class Point:
 
         EQ_THRESHOLD = 0.001
         return distance / FACTOR < EQ_THRESHOLD and self.intensity == other.intensity
-
-
-def to_float(chars: list[int]) -> float:
-    # バイト配列に変換
-    bytes_repr = bytes(chars)
-
-    # バイト配列をfloat型に変換
-    float_value = struct.unpack('f', bytes_repr)[0]
-
-    return float_value
 
 
 def create_timestamp_points_map(src_log_path: str) -> dict[str, list[Point]]:
@@ -46,10 +42,10 @@ def create_timestamp_points_map(src_log_path: str) -> dict[str, list[Point]]:
             POINT_STEP = 16
             points = []
             for i in range(POINT_DATA_OFFSET, len(message_contents)-1, POINT_STEP):
-                x = to_float([int(message_contents[j]) for j in range(i, i+4)])
-                y = to_float([int(message_contents[j]) for j in range(i+4, i+8)])
-                z = to_float([int(message_contents[j]) for j in range(i+8, i+12)])
-                intensity = to_float([int(message_contents[j]) for j in range(i+12, i+16)])
+                x = [int(message_contents[j]) for j in range(i, i+4)]
+                y = [int(message_contents[j]) for j in range(i+4, i+8)]
+                z = [int(message_contents[j]) for j in range(i+8, i+12)]
+                intensity = [int(message_contents[j]) for j in range(i+12, i+16)]
 
                 point = Point(x, y, z, intensity)
                 points.append(point)
@@ -77,17 +73,16 @@ if __name__ == "__main__":
 
         not_match_flag = False
         for before_point in before_map[before_ts]:
-            for after_point in after_map[before_ts]:
-                if before_point == after_point:
-                    continue
-            not_match_flag = True
+            if any(before_point == after_point for after_point in after_map[before_ts]):
+                continue
+            else:
+                not_match_flag = True
+                break
 
         if not_match_flag:
             num_not_match_messages += 1
-            print("Not match")
         else:
             num_match_messages += 1
-            print("Match")
 
     print('=== Result ===')
     print(f'Match: {num_match_messages}')
